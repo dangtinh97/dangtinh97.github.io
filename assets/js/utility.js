db53e5504f65172fcfc6020a78c34ba294822a93
dangtinh <dangtinha2@gmail.com>
2021-02-16 03:57:21 +0700

    test

---
+++ assets/js/utility.js
@@ -0,0 +1,31 @@
+const BASE_URL= 'https://web-app-5euxxskrra-uc.a.run.app'
+const API = {
+    user_post : BASE_URL+'/users',
+    user_search:BASE_URL+'/users',
+    send_message:BASE_URL+'/chats',
+    get_message:BASE_URL+'/chats',
+}
+
+let requestAjax =async (url,method,data=null,headers={})=>{
+    let res = {};
+    await $.ajax({
+        url:url,
+        type:method,
+        dataType:"JSON",
+        data:data,
+        headers: headers,
+        success:function (e){
+            res  =e;
+        }
+    }).fail(function (e){
+        console.log(e);
+        res  ={
+            success:false,
+            status:500,
+            content:'ERROR SERVER',
+            data:{}
+        }
+    })
+    return res
+}
+
