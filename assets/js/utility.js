f85f8fcb8eab9a0976ad2a0325a4158435e52977
dangtinh <dangtinha2@gmail.com>
2021-02-18 01:00:32 +0700

    start call

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,5 +1,6 @@
 const BASE_URL= 'https://web-app-5euxxskrra-uc.a.run.app'
 // const BASE_URL= 'http://supper-app.test'
+
 const API = {
     user_post : BASE_URL+'/users',
     user_search:BASE_URL+'/users',
