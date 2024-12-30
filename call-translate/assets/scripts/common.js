033898578bee7228cabf90578a598c58bb6c3254
dangtinhvu <dangtinha2@gmail.com>
2024-12-30 13:59:17 +0700

    call

--- call-translate/assets/scripts/common.js
+++ call-translate/assets/scripts/common.js
@@ -4,9 +4,9 @@ const randomNumberKey = (length = 4) => {
 }
 const configuration = {
   iceServers: [
-    // { urls: 'stun:stun.l.google.com:19302' }, // STUN server của Google,
-    // { urls: 'stun:stun1.l.google.com:19302' }, // STUN server của Google,
-    // { urls: 'stun:stun2.l.google.com:19302' }, // STUN server của Google,
-    { urls: 'turn:rtc.eyespy360.com:443?transport=tcp', credential: 'root', username: 'user' }
+    { urls: 'stun:stun.l.google.com:19302' }, // STUN server của Google,
+    { urls: 'stun:stun1.l.google.com:19302' }, // STUN server của Google,
+    { urls: 'stun:stun2.l.google.com:19302' }, // STUN server của Google,
+    { urls: 'turn:rtc.eyespy360.com:443?transport=tcp',username: 'user', credential: 'root' }
   ]
 }
