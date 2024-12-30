288a4a291e9694106499698e0847c47fa3aedddd
tinhvd <dangtinha2@gmail.com>
2024-12-30 11:56:46 +0700

    call-translate

--- call-translate/assets/scripts/common.js
+++ call-translate/assets/scripts/common.js
@@ -5,6 +5,8 @@ const randomNumberKey = (length=4)=>{
 const configuration = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" }, // STUN server của Google,
+    { urls: "stun:stun1.l.google.com:19302" }, // STUN server của Google,
+    { urls: "stun:stun2.l.google.com:19302" }, // STUN server của Google,
     {urls: "turn:rtc.eyespy360.com:443?transport=tcp", credential: "root", username: "user"}
   ]
 };
