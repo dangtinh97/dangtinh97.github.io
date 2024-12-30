1b84c090c06526ea8097a50bdbcc2d783987406b
tinhvd <dangtinha2@gmail.com>
2024-12-30 14:35:35 +0700

    call-translate

--- call-translate/assets/scripts/index.js
+++ call-translate/assets/scripts/index.js
@@ -1,117 +1,106 @@
-document.addEventListener("DOMContentLoaded",()=>{
-  const callNotification = document.getElementById("call-notification");
-  const localVideo = document.getElementById("localVideo");
-  const remoteVideo = document.getElementById("remoteVideo");
-  const keyOfMe = randomNumberKey(4);
-  $('#key-room > strong').html(keyOfMe);
-  let socket = io('https://websocket.dev.cabiz.ai/call-translate',{
-    transports: ['websocket','polling'],
-    auth:{
+document.addEventListener('DOMContentLoaded', () => {
+  const callNotification = document.getElementById('call-notification')
+  const localVideo = document.getElementById('localVideo')
+  const remoteVideo = document.getElementById('remoteVideo')
+  const keyOfMe = randomNumberKey(4)
+  $('#key-room > strong').html(keyOfMe)
+  let socket = io('https://websocket.dev.cabiz.ai/call-translate', {
+    transports: ['websocket', 'polling'],
+    auth: {
       key: keyOfMe
     }
   })
-  socket.on('connect',()=>{
-    console.log('socket connected: '+ socket.id);
-    socket.on('RINGING',(data)=>{
-      callNotification.style.display = "block";
+  socket.on('connect', () => {
+    console.log('socket connected: ' + socket.id)
+    socket.on('RINGING', (data) => {
+      callNotification.style.display = 'block'
     })
-    socket.on('DATA',async (data)=>{
-      console.log('DATA===>',data.type,data);
-      if(data.type==='answer'){
-        await peerConnection.setRemoteDescription(new RTCSessionDescription({
-          type: data.type,
-          sdp: data.sdp,
-        }));
+    socket.on('DATA', async (data) => {
+      console.log('DATA===>', data.type, data)
+      if (data.type === 'answer') {
+        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.sdp))
       }
-      if(data.type==='offer'){
-        handleOffer(data).then()
+      if (data.type === 'offer') {
+        handleOffer(data.sdp).then()
       }
-      if(data.type==='iceCandidate'){
+      if (data.type === 'candidate') {
         handleNewICECandidate(data.candidate).then()
       }
     })
   })
 
-  async function  handleOffer(offer) {
-    await getLocalStream();
-    createPeerConnection();
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+  async function handleOffer (offer) {
+    await getLocalStream()
+    createPeerConnection()
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer))
 
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
+    const answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
 
-    socket.emit('DATA',{
+    socket.emit('DATA', {
       type: 'answer',
-      sdp: peerConnection.localDescription.sdp,
+      sdp: answer,
       key: keyOfMe,
     })
   }
 
-  $('#joinRoomButton').on('click',()=>{
-    const keyJoin = $('#roomInput').val();
-    socket.emit('CONNECT_WITH_GUEST',{
+  $('#joinRoomButton').on('click', () => {
+    const keyJoin = $('#roomInput').val()
+    socket.emit('CONNECT_WITH_GUEST', {
       key: keyJoin,
-    });
+    })
   })
 
-  $('#acceptCallButton').on('click',()=>{
-    callNotification.style.display = "none";
-    getLocalStream().then(async ()=>{
+  $('#acceptCallButton').on('click', () => {
+    callNotification.style.display = 'none'
+    getLocalStream().then(async () => {
       createPeerConnection()
-      const offer = await peerConnection.createOffer();
-      await peerConnection.setLocalDescription(offer);
-      socket.emit('DATA',{
+      const offer = await peerConnection.createOffer()
+      await peerConnection.setLocalDescription(offer)
+      socket.emit('DATA', {
         type: 'offer',
-        sdp: peerConnection.localDescription.sdp,
+        sdp: offer,
         key: keyOfMe,
-      });
-    });
+      })
+    })
   })
-  let localStream;
-  let peerConnection;
-  let remoteStream;
-  const getLocalStream =async ()=>{
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    localStream = stream;
-    localVideo.srcObject = stream;
+  let localStream
+  let peerConnection
+  let remoteStream
+  const getLocalStream = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    localStream = stream
+    localVideo.srcObject = stream
   }
 
-  function createPeerConnection() {
-    peerConnection = new RTCPeerConnection(configuration);
-    // Khi nhận được stream từ đối tác
+  function createPeerConnection () {
+    peerConnection = new RTCPeerConnection(configuration)
     peerConnection.ontrack = (event) => {
-      remoteStream = event.streams[0];
-      remoteVideo.srcObject = remoteStream;
-
-    };
-
-    // Gửi các luồng media cục bộ tới đối tác
-    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
-
-    // Thiết lập ICE candidates
+      remoteStream = event.streams[0]
+      remoteVideo.srcObject = remoteStream
+    }
+    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream))
     peerConnection.onicecandidate = (event) => {
       if (event.candidate) {
-        // Gửi ICE candidate tới signaling server (gửi qua WebSocket)
-        console.log("New ICE Candidate:", event.candidate);
-        socket.emit('DATA',{
-          type: 'iceCandidate',
+        socket.emit('DATA', {
+          type: 'candidate',
           candidate: event.candidate,
           key: keyOfMe,
-        });
+        })
       }
-    };
+    }
     peerConnection.oniceconnectionstatechange = () => {
-      console.log("ICE state:", peerConnection.iceConnectionState);
-    };
+      console.log('ICE state:', peerConnection.iceConnectionState)
+    }
   }
 
-  async function handleNewICECandidate(candidate) {
-    if(peerConnection){
-      await peerConnection.addIceCandidate(new RTCIceCandidate(candidate)).then(()=>{
-        console.log("ICE Candidate added")
-      }).catch(()=>{
-        console.log("ICE Candidate failed to add: ", candidate.candidate.replace(/[\n\r]/g, ""))
-      });
+  async function handleNewICECandidate (candidate) {
+    if (peerConnection) {
+      await peerConnection.addIceCandidate(new RTCIceCandidate(candidate)).then(() => {
+        console.log('ICE Candidate added')
+      }).catch(() => {
+        console.log('ICE Candidate failed to add: ', candidate.candidate.replace(/[\n\r]/g, ''))
+      })
     }
   }
 })
