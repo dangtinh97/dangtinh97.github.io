bf2c0898b4299c490c175b4df2698f1e948a43ed
dangtinh97 <36624269+dangtinh97@users.noreply.github.com>
2026-06-02 08:04:11 +0700

    Create config.js

---
+++ fcm-demo/scripts/config.js
@@ -0,0 +1,11 @@
+const FCM_VAPID_PUBLIC_KEY = "BCjwUI5OBGhqei9Dn_FOSqSB-BMq0D2QRziQu0xqAgIndjj_CVr_V0L222C5uLucrhIHr7OCG3-D9HjefPxS_dU";
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBYWNoJbnLJSMa1ScBuefwhJESMgHfSQ_g",
+    authDomain: "iwedding-345f9.firebaseapp.com",
+    databaseURL: "https://iwedding-345f9-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "iwedding-345f9",
+    storageBucket: "iwedding-345f9.firebasestorage.app",
+    messagingSenderId: "942576843754",
+    appId: "1:942576843754:web:72ce049bb7d4da7b2e5093",
+    measurementId: "G-B2KD2RZ28Q"
+}
